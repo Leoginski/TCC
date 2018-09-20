ba03adffb41eda488c514dc005cf46d914eaa73b
Leoginski <leonardo.smoginski@gmail.com>
2018-09-19 22:28:51 -0300

    DownloadScript - Jupyter

--- PyScript/CleanData.py
+++ PyScript/CleanData.py
@@ -1,5 +1,7 @@
 import os
 import pandas as pd
 
-csv_data = pd.read_csv('../DataSet/201301_ItemLicitaç╞o.csv', sep=';', encoding='latin-1', skiprows=1)
-csv_data.to_csv('../DataSet/teste.csv', sep=';', encoding='utf-8')
+data = pd.read_csv('/home/leoginski/Modelos/TCC/DataSet/201301_ItemLicitaç╞o.csv', sep=';', encoding='latin-1', skiprows=1)
+data.head()
+
+# csv_data.to_csv('../DataSet/teste.csv', sep=';', encoding='utf-8')
